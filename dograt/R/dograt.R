67e8f152e2fcce4d6c186680263cc8b1ecf1dd28
Ryan <ryangreen@ucsb.edu>
2024-08-16 13:30:20 -0700

    added package creation notes

--- dograt/R/dograt.R
+++ dograt/R/dograt.R
@@ -20,6 +20,8 @@ hello <- function() {
 
 # when creating a new package, you'll start from a template package from R called 'hello'. Once you make changes, you have to reload the whole package to get those changes. To do that, go to the Build tab by the environment, and hit Install! This will restart R and your changed package should be functional.
 
+# once you create a package, you can add metadata, or instructions you can load by typing in the console ?(packagename). To add this metadata, place your cursor at the beginning of the function, and go to Code > Insert Roxygen Skeleton, and fill out that info how you like. Once you've done that, hit Install in the Build tab. This will reload R. Then in the console, type devtools::document() and it will update your package with the metadata you added. Then hit Install again and it should work-- call your package in the console using ?(packagename) to see it.
+
 
 
 

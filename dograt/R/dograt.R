91b96e43df3b88edcf92e7e7b23c0e7e6ffc8fb2
Ryan <ryangreen@ucsb.edu>
2024-08-16 13:27:53 -0700

    Initial commit

---
+++ dograt/R/dograt.R
@@ -0,0 +1,25 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello my name is dograt I am both rat and dog")
+  print("kachow")
+}
+
+# when creating a new package, you'll start from a template package from R called 'hello'. Once you make changes, you have to reload the whole package to get those changes. To do that, go to the Build tab by the environment, and hit Install! This will restart R and your changed package should be functional.
+
+
+
+
---
+++ dograt/R/spiderman.R
@@ -0,0 +1,23 @@
+# make a function with three arguments, at least one numeric and one character. Save the package and run it!
+
+
+#' Spider-Man
+#'
+#' @param people_saved total number of people Spider-man saved
+#' @param saved_from what did Spider-man save the people from?
+#' @param day Was this just a regular old Tuesday?
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' spiderman(9, "Venom", "a regular old Tuesday")
+#'
+#'
+spiderman <- function(people_saved, saved_from, day){
+  print(paste("Spider-man saved", people_saved, "people from", saved_from, "on", day))
+}
+
+
+
+
